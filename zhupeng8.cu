50df94df75a423cde97e6992dea482241fa3fc6c
penghui.zhu <zhupeng8@ug61.eecg.toronto.edu>
2024-02-02 20:22:29 -0500

    cpu to **

--- zhupeng8.cu
+++ zhupeng8.cu
@@ -2,60 +2,67 @@
 #include <unistd.h>
 #include <vector>
 
-inline int dimension_convert(int x, int y, int cols) { return x * cols + y; }
-
-void print_matrix(float *m, int rows, int cols) {
+void print_matrix(float **m, int rows, int cols) {
   for (int i = 0; i < rows; ++i) {
     for (int j = 0; j < cols; ++j) {
-      printf("%lf,", m[i * cols + j]);
+      printf("%lf,", m[i][j]);
     }
     printf("\n");
   }
 }
 
-void gen_matrix(int rows, int cols, float *x, float *y) {
+void gen_matrix(int rows, int cols, float **x, float **y) {
   for (int i = 0; i < rows; ++i) {
     for (int j = 0; j < cols; ++j) {
-      x[i * cols + j] = (float)((i + j) % 100) / 2.0;
-      y[i * cols + j] = (float)3.25 * ((i + j) % 100);
+      printf("%d,%d\n", i, j);
+      x[i][j] = (float)((i + j) % 100) / 2.0;
+      y[i][j] = (float)3.25 * ((i + j) % 100);
     }
   }
 }
 
-void cpu_calculate(float *x, float *y, int rows, int cols, float *z) {
+void cpu_malloc(float ***m, int rows, int cols) {
+  *m = (float **)malloc(rows * sizeof(float *));
+  for (size_t i = 0; i < rows; i++) {
+    (*m)[i] = (float *)malloc(cols * sizeof(float));
+  }
+}
+
+void cpu_free(float **m, int rows) {
+  for (int i = 0; i < rows; ++i) {
+    free(m[i]);
+  }
+  free(m);
+}
+
+void cpu_calculate(float **x, float **y, int rows, int cols, float **z) {
   for (int i = 0; i < rows; ++i) {
     for (int j = 0; j < cols; ++j) {
-      z[dimension_convert(i, j, cols)] =
-          (i == 0 ? 0 : x[dimension_convert(i, j, cols)]) +
-          x[dimension_convert(i, j, cols)] +
-          (i == rows - 1 ? 0 : x[dimension_convert(i + 1, j, cols)]) -
-          (j < 2 ? 0 : y[dimension_convert(i, j - 2, cols)]) -
-          (j < 1 ? 0 : y[dimension_convert(i, j - 1, cols)]) -
-          y[dimension_convert(i, j, cols)];
+      z[i][j] = (i == 0 ? 0 : x[i - 1][j]) + x[i][j] +
+                (i == rows - 1 ? 0 : x[i + 1][j]) - (j < 2 ? 0 : y[i][j - 2]) -
+                (j < 1 ? 0 : y[i][j - 1]) - y[i][j];
     }
   }
 }
 
-void cpy_and_calculate(float *x, float *y, int rows, int cols) {
+void cpy_and_calculate(float **x, float **y, int rows, int cols) {
   // malloc and copy to device
-  float *d_x, *d_y;
+  float **d_x, **d_y;
   int elements = rows * cols;
 
-  cudaMalloc((void **)&d_x, elements * sizeof(float));
-  cudaMalloc((void **)&d_y, elements * sizeof(float));
-  cudaMemcpy(d_x, x, elements, cudaMemcpyHostToDevice);
-
   // CPU calculation
-  float *cpu_z = static_cast<float *>(malloc(rows * cols * sizeof(float)));
+  float **cpu_z;
+  cpu_malloc(&cpu_z, rows, cols);
   cpu_calculate(x, y, rows, cols, cpu_z);
   print_matrix(cpu_z, rows, cols);
+  cpu_free(cpu_z, rows);
 
-  float *d_z;
-  cudaMalloc((void **)&d_z, elements * sizeof(float));
+  // float *d_z;
+  // cudaMalloc((void **)&d_z, elements * sizeof(float));
 
-  cudaFree(d_x);
-  cudaFree(d_y);
-  cudaFree(d_z);
+  // cudaFree(d_x);
+  // cudaFree(d_y);
+  // cudaFree(d_z);
 }
 
 int main(int argc, char *argv[]) {
@@ -87,14 +94,17 @@ int main(int argc, char *argv[]) {
 
   printf("specified rows:%d, cols:%d\n", rows, cols);
 
-  float *x, *y;
-  x = static_cast<float *>(malloc(rows * cols * sizeof(float)));
-  y = static_cast<float *>(malloc(rows * cols * sizeof(float)));
+  float **x, **y;
+  cpu_malloc(&x, rows, cols);
+  cpu_malloc(&y, rows, cols);
 
   gen_matrix(rows, cols, x, y);
 
   print_matrix(x, rows, cols);
   print_matrix(y, rows, cols);
+
+  cpu_free(x, rows);
+  cpu_free(y, rows);
 
   return 0;
 }
